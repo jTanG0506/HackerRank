383af94dcaf31b33199c44cfc62ed254cd0951ec
Jonathan Tang <jonathan.tang@student.manchester.ac.uk>
2018-03-27 16:22:17 +0100

    Complete 'java-stdin-and-stdout-i'

---
+++ java/introduction/java-stdin-and-stdout-i/Solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int first = scanner.nextInt();
+    int second = scanner.nextInt();
+    int third = scanner.nextInt();
+
+    System.out.println(first);
+    System.out.println(second);
+    System.out.println(third);
+  }
+
+}
