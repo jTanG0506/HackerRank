57f0d36cd449707837945ca2b8bc9b1247347abf
Jonathan Tang <jonathan.tang@student.manchester.ac.uk>
2018-03-27 22:08:09 +0100

    Complete 'java-currency-formatter'

---
+++ java/introduction/java-currency-formatter/Solution.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.Locale;
+import java.text.NumberFormat;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        String us = NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+        Locale in = new Locale("en", "IN");
+        String india = NumberFormat.getCurrencyInstance(in).format(payment);
+        String china = NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment);
+        String france = NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment);
+
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
