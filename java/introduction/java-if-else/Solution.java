787235163eb10ee77ea56933f3fda3c9361b1d58
Jonathan Tang <jonathan.tang@student.manchester.ac.uk>
2018-03-27 16:22:25 +0100

    Complete 'java-if-else'

---
+++ java/introduction/java-if-else/Solution.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Solution {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n = scanner.nextInt();
+
+    String ans = "";
+
+    if (n % 2 == 0) {
+      if (n >= 2 && n <= 5) {
+        ans = "Not Weird";
+      } else if (n >= 6 && n <= 20) {
+        ans = "Weird";
+      } else {
+        ans = "Not Weird";
+      }
+    } else {
+      ans = "Weird";
+    }
+
+    System.out.println(ans);
+  }
+
+}
