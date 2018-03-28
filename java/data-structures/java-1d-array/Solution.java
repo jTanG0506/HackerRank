247be74ea32ff1babe4ff35bce24e18623128496
Jonathan Tang <jonathan.tang@student.manchester.ac.uk>
2018-03-28 08:10:19 +0100

    Complete 'java-1d-array'

---
+++ java/data-structures/java-1d-array/Solution.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Solution {
+
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int n = scan.nextInt();
+
+    int[] arr = new int[n];
+    for (int i = 0; i < arr.length; i++) {
+      arr[i] = scan.nextInt();
+    }
+
+    scan.close();
+
+    for (int i = 0; i < arr.length; i++) {
+      System.out.println(arr[i]);
+    }
+  }
+
+}
